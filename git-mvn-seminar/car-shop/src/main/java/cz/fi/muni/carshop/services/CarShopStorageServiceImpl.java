e300145922db713d1a29fc32a5cc8c53c88a6a8e
Lenka Horváthová <xhorvat8@fi.muni.cz>
2018-09-25 13:49:25 +0200

    Updated pom.xml. Added an implementation of sellCar() method.

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -30,7 +31,24 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 
 	@Override
 	public void addCarToStorage(Car car) {
+		if (car.getPrice() < 0) {
+			throw new IllegalArgumentException();
+		}
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
 	}
 
+	@Override
+	public void sellCar(Car car) throws RequestedCarNotFoundException {
+		Map<CarTypes, List<Car>> allCars = CarShopStorage.getInstancce().getCars();
+		List<Car> carsOfSameType = allCars.get(car.getType());
+		for (Car searchedCar: carsOfSameType) {
+			if ((searchedCar.getPrice() == car.getPrice())
+					&& (searchedCar.getColor() == car.getColor())
+					&& (searchedCar.getConstructionYear() == car.getConstructionYear()))  {
+				CarShopStorage.getInstancce().getCars().remove(searchedCar);
+			} else {
+				throw new RequestedCarNotFoundException("Car was't found in the storage.");
+			}
+		}
+	}
 }
