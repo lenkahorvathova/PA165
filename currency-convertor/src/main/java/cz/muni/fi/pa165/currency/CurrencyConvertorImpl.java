e36db3d0ae0a93cdac75b034565a758f16120bc5
Lenka Horváthová <xhorvat8@fi.muni.cz>
2018-10-02 13:46:02 +0200

    An unfinished addition of a logger.

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
@@ -1,5 +1,8 @@
 package cz.muni.fi.pa165.currency;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Currency;
@@ -13,7 +16,7 @@ import java.util.Currency;
 public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     private final ExchangeRateTable exchangeRateTable;
-    //private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
 
     public CurrencyConvertorImpl(ExchangeRateTable exchangeRateTable) {
         this.exchangeRateTable = exchangeRateTable;
@@ -21,18 +24,22 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
+        logger.trace("convert({},{},{})",sourceCurrency, targetCurrency, sourceAmount);
+
         if (sourceCurrency == null || targetCurrency == null || sourceAmount == null) {
             throw new IllegalArgumentException("Some Input data are missing. Please, check all values!");
         }
         try {
             BigDecimal exchangeRate = exchangeRateTable.getExchangeRate(sourceCurrency, targetCurrency);
             if (exchangeRate == null) {
+                logger.warn("No exchange rate for these currencies.");
                 throw new UnknownExchangeRateException("No exchange rate for these currencies.");
             }
 
             return exchangeRate.multiply(sourceAmount).setScale(2, RoundingMode.HALF_EVEN);
 
         } catch (ExternalServiceFailureException e) {
+            logger.error("Error occured.");
             throw new UnknownExchangeRateException("Error occured.", e);
         }
     }
