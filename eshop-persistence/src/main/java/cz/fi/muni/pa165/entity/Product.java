8de5f1375d194f104f80b768a9233b360083fdb5
Lenka Horváthová <xhorvat8@fi.muni.cz>
2018-10-23 13:48:32 +0200

    Unfinished Task02.

--- eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
+++ eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
@@ -28,175 +28,146 @@ import cz.fi.muni.pa165.dto.Color;
 import cz.fi.muni.pa165.validation.AllOrNothing;
 
 @Entity
-@AllOrNothing(members={"image", "imageMimeType"})
+@AllOrNothing(members = {"image", "imageMimeType"})
 public class Product {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	
 
-	@Lob
-	private byte[] image;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	private String imageMimeType;
-	
+    @Lob
+    private byte[] image;
 
-	private String name;
-	
-	/*
-	 * The day this item has been added to the eshop
-	 */
-	private LocalDate addedDate;
-	
+    private String imageMimeType;
 
-	@OneToOne
-	@JoinTable(name="CURRENT_PRICE")
-	private Price currentPrice;
-	
-	@OneToMany()
-	@OrderBy("priceStart DESC")
-	@JoinColumn(name="Product_FK")
-	private List<Price> priceHistory = new ArrayList<Price>();
-	
-	@Enumerated
-	private Color color;
+    private String name;
 
-	
-	public void setId(Long id){
-		this.id = id;
-	}
+    /*
+     * The day this item has been added to the eshop
+     */
+    private LocalDate addedDate;
 
+    @ManyToMany
+    private Set<Category> categories = new HashSet<Category>();
 
+    @OneToOne
+    @JoinTable(name = "CURRENT_PRICE")
+    private Price currentPrice;
 
-	/**
-	 * TODO these two methods are here just to make Task04 compilable. After you are finished
-	 * with TASK 02 you should delete this empty method
-	 * @param kitchen
-	 */
-	public void addCategory(Category kitchen) {	
-	}
-	public List<Product> getCategories() {
-		return null;
-	}
-	//TODO after you are done with task02 you can uncomment this methods
-//	public void removeCategory(Category category)	{
-//		this.categories.remove(category);
-//	}
-//	
-//	public void addCategory(Category c) {
-//		categories.add(c);
-//		c.addProduct(this);
-//	}
-//
-//	public Set<Category> getCategories() {
-//		return Collections.unmodifiableSet(categories);
-//	}
+    @OneToMany()
+    @OrderBy("priceStart DESC")
+    @JoinColumn(name = "Product_FK")
+    private List<Price> priceHistory = new ArrayList<Price>();
 
+    @Enumerated
+    private Color color;
 
-	public LocalDate getAddedDate() {
-		return addedDate;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setAddedDate(LocalDate addedDate) {
-		this.addedDate = addedDate;
-	}
+    public void removeCategory(Category category) {
+        this.categories.remove(category);
+    }
 
-	public Product(Long productId) {
-		this.id = productId;
-	}
-	public Product() {
-	}
-	public byte[] getImage() {
-		return image;
-	}
-	
+    public void addCategory(Category c) {
+        categories.add(c);
+        c.addProduct(this);
+    }
 
-	public String getImageMimeType() {
-		return imageMimeType;
-	}
+    public Set<Category> getCategories() {
+        return Collections.unmodifiableSet(categories);
+    }
 
+    public LocalDate getAddedDate() {
+        return addedDate;
+    }
 
+    public void setAddedDate(LocalDate addedDate) {
+        this.addedDate = addedDate;
+    }
 
-	public void setImageMimeType(String imageMimeType) {
-		this.imageMimeType = imageMimeType;
-	}
+    public Product(Long productId) {
+        this.id = productId;
+    }
 
+    public Product() {
+    }
 
+    public byte[] getImage() {
+        return image;
+    }
 
-	public Price getCurrentPrice() {
-		return currentPrice;
-	}
+    public String getImageMimeType() {
+        return imageMimeType;
+    }
 
+    public void setImageMimeType(String imageMimeType) {
+        this.imageMimeType = imageMimeType;
+    }
 
-	public void addHistoricalPrice(Price p){
-		priceHistory.add(p);
-	}
-	
-	public void setCurrentPrice(Price currentPrice) {
-		this.currentPrice = currentPrice;
-	}
+    public Price getCurrentPrice() {
+        return currentPrice;
+    }
 
+    public void addHistoricalPrice(Price p) {
+        priceHistory.add(p);
+    }
 
-	public List<Price> getPriceHistory() {
-		return Collections.unmodifiableList(priceHistory);
-	}
+    public void setCurrentPrice(Price currentPrice) {
+        this.currentPrice = currentPrice;
+    }
 
+    public List<Price> getPriceHistory() {
+        return Collections.unmodifiableList(priceHistory);
+    }
 
-	public void setImage(byte[] image) {
-		this.image = image;
-	}
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 
+    public String getName() {
+        return name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Color getColor() {
+        return color;
+    }
 
-	public Color getColor() {
-		return color;
-	}
+    public void setColor(Color color) {
+        this.color = color;
+    }
 
-	public void setColor(Color color) {
-		this.color = color;
-	}
+    public Long getId() {
+        return id;
+    }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
 
-	public Long getId() {
-		return id;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof Product))
-			return false;
-		Product other = (Product) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.getName()))
-			return false;
-		return true;
-	}
-
-
-
-	
-	
-	
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Product))
+            return false;
+        Product other = (Product) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.getName()))
+            return false;
+        return true;
+    }
 }
--- eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task02.java
+++ eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task02.java
@@ -17,33 +17,67 @@ import cz.fi.muni.pa165.PersistenceSampleApplicationContext;
 import cz.fi.muni.pa165.entity.Category;
 import cz.fi.muni.pa165.entity.Product;
 
- 
+
 @ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
 public class Task02 extends AbstractTestNGSpringContextTests {
 
-	@PersistenceUnit
-	private EntityManagerFactory emf;
+    @PersistenceUnit
+    private EntityManagerFactory emf;
 
-	
-	private void assertContainsCategoryWithName(Set<Category> categories,
-			String expectedCategoryName) {
-		for(Category cat: categories){
-			if (cat.getName().equals(expectedCategoryName))
-				return;
-		}
-			
-		Assert.fail("Couldn't find category "+ expectedCategoryName+ " in collection "+categories);
-	}
-	private void assertContainsProductWithName(Set<Product> products,
-			String expectedProductName) {
-		
-		for(Product prod: products){
-			if (prod.getName().equals(expectedProductName))
-				return;
-		}
-			
-		Assert.fail("Couldn't find product "+ expectedProductName+ " in collection "+products);
-	}
+    private Category electro = new Category();
+    private Category kitchen = new Category();
+    private Product flashlight = new Product();
+    private Product kitchenRobot = new Product();
+    private Product plate = new Product();
 
-	
+    @BeforeClass
+    public void createTestData() {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        electro.setName("Electro");
+        em.persist(electro);
+
+        kitchen.setName("Kitchen");
+        em.persist(kitchen);
+
+        flashlight.setName("Flashlight");
+        flashlight.addCategory(electro);
+        em.persist(flashlight);
+
+        kitchenRobot.setName("Kitchen Robot");
+        kitchenRobot.addCategory(electro);
+        kitchenRobot.addCategory(kitchen);
+        em.persist(kitchenRobot);
+
+        plate.setName("Plate");
+        plate.addCategory(kitchen);
+        em.persist(plate);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    private void assertContainsCategoryWithName(Set<Category> categories,
+                                                String expectedCategoryName) {
+        for (Category cat : categories) {
+            if (cat.getName().equals(expectedCategoryName))
+                return;
+        }
+
+        Assert.fail("Couldn't find category " + expectedCategoryName + " in collection " + categories);
+    }
+
+    private void assertContainsProductWithName(Set<Product> products,
+                                               String expectedProductName) {
+
+        for (Product prod : products) {
+            if (prod.getName().equals(expectedProductName))
+                return;
+        }
+
+        Assert.fail("Couldn't find product " + expectedProductName + " in collection " + products);
+    }
+
+
 }
